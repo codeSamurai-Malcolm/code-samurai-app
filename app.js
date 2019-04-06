890584c02e790cd4d26991e95be398e05ce1f572
malcom@codesamurai.club <malcom@codesamurai.club>
2019-04-06 13:39:33 -0400

    push

--- app.js
+++ app.js
@@ -106,12 +106,19 @@ app.post('/login', function (request, response) {
     var password = request.body.password;
 
     // save login name in session so it's available later
-    request.session.user = loginName;
+    //request.session.user = loginName;
 
     //hint: check is password is good or not, if not load same page with error as below
     //response.render('index', {message: "Invalid user name or password"});
 
-    response.render('listpage', {items: Item.find()});
+    if(userPasswordMatch(loginName,password)){
+        request.session.user=loginName;
+        response.render('listpage',{items:Item.find()});
+    } else {
+         response.render('index',{message: "Invalid user name or Password"});
+    }
+
+
 
 });
 
@@ -122,7 +129,7 @@ app.post('/saveitem', function (request, response) {
 
     // hint #1: find the helper function that will help save the information first
     // hint #2: make sure to send the list of items to the list page
-
-    response.render('listpage',{ items:[] });
+var games = saveFormAndReturnAllItems(request.body)
+    response.render('listpage',{ items:games });
 });
 
